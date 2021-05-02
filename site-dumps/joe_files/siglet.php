2dab55cacc3a85ee4262627cd32572887be9ac5d
rockykev <kev.rocky@gmail.com>
2021-05-01 20:11:25 -0700

    every slide has something

---
+++ site-dumps/joe_files/siglet.php
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+document.write('<TABLE width="240" border=1 cellpadding=3 cellspacing=0 bgcolor=White>')
+document.write('<TR><TD><FONT COLOR="Black" face="Helvetica,Arial" size=2><div align="center"><B>Siglet of the Moment</B></div><BR><B>')
+document.write('Born Free. . . . .Taxed to Death.</B></FONT><BR><BR><div align="center"><A HREF="https://web.archive.org/web/20021029063934/http://www.siglets.com" TARGET="_blank"><img src="https://web.archive.org/web/20021029063934/http://www.siglets.com/linkimages/siglets88b.gif" width="88" height="31" alt="Free Siglets!"></A></div>')
+document.write('</TD></TR></TABLE>')
+document.close()
+
+}
+/*
+     FILE ARCHIVED ON 06:39:34 Oct 29, 2002 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:29:36 May 02, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 185.104
+  exclusion.robots: 0.083
+  exclusion.robots.policy: 0.076
+  RedisCDXSource: 7.715
+  esindex: 0.009
+  LoadShardBlock: 158.637 (3)
+  PetaboxLoader3.datanode: 129.251 (4)
+  CDXLines.iter: 16.738 (3)
+  load_resource: 107.406
+  PetaboxLoader3.resolve: 49.15
+*/
---
+++ site-dumps/linn_storvik_files/show_ad.php
@@ -0,0 +1,3 @@
+<!--
+document.write("<a href=\"https://ad.broadcaststation.net/ads/url.php?id=CLOUD9\" target=\"_blank\"><img src=\"https://ad.broadcaststation.net/ads/images/728x90_vps9.png\" alt=\"CLOUD9\" width=\"728\" height=\"90\" border=\"0\" /></a>");
+//-->
